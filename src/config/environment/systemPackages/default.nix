c56c7e572b63d73e260e5caa9ee1d4bd108a122a
Diego Restrepo <restrepomesadiego@gmail.com>
2021-07-05 14:29:23 -0500

    refac(back): remove programs

--- src/config/environment/systemPackages/default.nix
+++ src/config/environment/systemPackages/default.nix
@@ -4,57 +4,34 @@ _: with _; [
   packages.nixpkgs.bind
   packages.nixpkgs.binutils
   packages.nixpkgs.black
-  packages.nixpkgs.burpsuite
-  packages.nixpkgs.cabal-install
   packages.nixpkgs.cachix
-  packages.nixpkgs.cargo
   packages.nixpkgs.coreutils
   packages.nixpkgs.curl
   packages.nixpkgs.dconf2nix
-  packages.nixpkgs.diction
-  packages.nixpkgs.diffoscope
   packages.nixpkgs.dmenu
   packages.nixpkgs.gcc
   packages.nixpkgs.ghc
   packages.nixpkgs.gnome.dconf-editor
   packages.nixpkgs.gnumake
-  packages.nixpkgs.google-chrome
   packages.nixpkgs.gnupg
   packages.nixpkgs.hugo
   packages.nixpkgs.jq
   packages.nixpkgs.just
-  packages.nixpkgs.kubectl
-  packages.nixpkgs.libreoffice
   packages.nixpkgs.lsof
-  packages.nixpkgs.maven
   packages.nixpkgs.mypy
-  packages.nixpkgs.ngrok
   packages.nixpkgs.niv
   packages.nixpkgs.nix-index
   packages.nixpkgs.nix-info
   packages.nixpkgs.nixos-generators
   packages.nixpkgs.nixpkgs-fmt
   packages.nixpkgs.nixpkgs-review
-  packages.nixpkgs.nodejs
-  packages.nixpkgs.openjdk
   packages.nixpkgs.optipng
   packages.nixpkgs.parallel
   packages.nixpkgs.parted
-  packages.nixpkgs.patchelf
-  packages.nixpkgs.pcre
-  packages.nixpkgs.peek
   packages.nixpkgs.python38
   packages.nixpkgs.python38Packages.isort
-  packages.nixpkgs.qemu
-  packages.nixpkgs.shadow
   packages.nixpkgs.shfmt
   packages.nixpkgs.sops
-  packages.nixpkgs.terraform
-  packages.nixpkgs.tokei
-  packages.nixpkgs.torbrowser
-  packages.nixpkgs.traceroute
-  packages.nixpkgs.tree
-  packages.nixpkgs.vlc
   packages.nixpkgs.vscode
   packages.nixpkgs.wirelesstools
   packages.nixpkgs.wpa_supplicant
