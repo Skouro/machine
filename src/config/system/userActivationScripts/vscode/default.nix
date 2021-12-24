52dabdea7c64f89376ad2baef2961884b0b6e3de
Diego Restrepo <restrepomesadiego@gmail.com>
2021-12-24 16:02:15 -0500

    feat(back): add vscode extensions

--- src/config/system/userActivationScripts/vscode/default.nix
+++ src/config/system/userActivationScripts/vscode/default.nix
@@ -4,7 +4,6 @@ _: with _; utils.script {
       name = "extensions";
       paths = [
         packages.nixpkgs.vscode-extensions._4ops.terraform
-        packages.nixpkgs.vscode-extensions.bbenoist.Nix
         packages.nixpkgs.vscode-extensions.coenraads.bracket-pair-colorizer-2
         packages.nixpkgs.vscode-extensions.coolbear.systemd-unit-file
         packages.nixpkgs.vscode-extensions.jkillian.custom-local-formatters
@@ -28,6 +27,24 @@ _: with _; utils.script {
           sha256 = "0gj71xy7r82n1pic00xsi04dg7zg0dsxx000s03iq6lnz47s84gn";
         }
         {
+          name = "haskell";
+          publisher = "haskell";
+          version = "1.6.1";
+          sha256 = "1l6nrbqkq1p62dkmzs4sy0rxbid3qa1104s3fd9fzkmc1sldzgsn";
+        }
+        {
+          name = "language-haskell";
+          publisher = "justusadam";
+          version = "3.4.0";
+          sha256 = "0ab7m5jzxakjxaiwmg0jcck53vnn183589bbxh3iiylkpicrv67y";
+        }
+        {
+          name = "rust";
+          publisher = "rust-lang";
+          version = "0.7.8";
+          sha256 = "039ns854v1k4jb9xqknrjkj8lf62nfcpfn0716ancmjc4f0xlzb3";
+        }
+        {
           name = "vscodeintellicode";
           publisher = "visualstudioexptteam";
           version = "1.2.14";
@@ -36,14 +53,14 @@ _: with _; utils.script {
         {
           name = "vscode-pylance";
           publisher = "ms-python";
-          version = "2021.9.1";
-          sha256 = "08lijgnyrn1vbs5aqb18w0xzzw6n4ss2m8a8zwyjk62l01ngmffg";
+          version = "2021.11.2";
+          sha256 = "0di2skybp8jw3apyd74ifsrsp02hr84j92jpgkipdqyqmis4bbmm";
         }
         {
           name = "python";
           publisher = "ms-python";
-          version = "2021.8.1159798656";
-          sha256 = "030si91s53ii9mqwzf4djsdh7ranaky3x9m26s279n6g9i7vd3r7";
+          version = "2021.11.1422169775";
+          sha256 = "1ax0lr0r9ip8pb3fzg462c00q98rlnpq5al5fkgaywcyx2k9pib3";
         }
         {
           name = "rest-client";
@@ -60,14 +77,14 @@ _: with _; utils.script {
         {
           name = "gitlens";
           publisher = "eamodio";
-          version = "11.5.1";
-          sha256 = "1c0kfavdwgwham92xrh0gnyxkrl9qlkpv39l1yhrldn8vd10fj5i";
+          version = "11.7.0";
+          sha256 = "0apjjlfdwljqih394ggz2d8m599pyyjrb0b4cfcz83601b7hk3x6";
         }
         {
           name = "vscode-icons";
           publisher = "vscode-icons-team";
-          version = "11.6.0";
-          sha256 = "07mp6q7xy9r53b1bh8gaw6kz64m4bzyxzm095105vv1p5l78b3lf";
+          version = "11.7.0";
+          sha256 = "06a0br8szsf3i72hhq233p08b5zp7wdb8nn29h1gblhgzmv1p830";
         }
         {
           name = "magicpython";
@@ -80,6 +97,36 @@ _: with _; utils.script {
           publisher = "mohsen1";
           version = "0.0.3";
           sha256 = "1spj01dpfggfchwly3iyfm2ak618q2wqd90qx5ndvkj3a7x6rxwn";
+        }
+        {
+          name = "jupyter";
+          publisher = "ms-toolsai";
+          version = "2021.9.1001220611";
+          sha256 = "0yzmqyzh3c0vwxl4w5i5cvi1vc5y37szjllk1rfg5hivh6d0py54";
+        }
+        {
+          name = "gitlens";
+          publisher = "eamodio";
+          version = "11.6.0";
+          sha256 = "0lhrw24ilncdczh90jnjx71ld3b626xpk8b9qmwgzzhby89qs417";
+        }
+        {
+          name = "Nix";
+          publisher = "bbenoist";
+          version = "1.0.1";
+          sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+        }
+        {
+          name = "nix-env-selector";
+          publisher = "arrterian";
+          version = "1.0.7";
+          sha256 = "0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
+        }
+        {
+          name = "vscode-java-pack";
+          publisher = "vscjava";
+          version = "0.18.7";
+          sha256 = "0klivkkhp7xb1s6nqkl3r1gac21mdpdgz9f381lk1321rzvnr0gm";
         }
       ];
     };
@@ -100,12 +147,12 @@ _: with _; utils.script {
 
               PYTHONPATH=/pythonpath/not/set
 
-              ${packages.nixpkgs.black}/bin/black \
+              ${packages.pythonOnNix.projects.black.latest.pythonLatest.bin}/bin/black \
                 --config \
                 ${sources.product}/makes/utils/python-format/settings-black.toml \
                 - \
                 | \
-              ${packages.nixpkgs.python38Packages.isort}/bin/isort \
+              ${packages.pythonOnNix.projects.isort.latest.pythonLatest.bin}/bin/isort \
                 --settings-path \
                 ${sources.product}/makes/utils/python-format/settings-isort.toml \
                 -
@@ -158,14 +205,14 @@ _: with _; utils.script {
         "${sources.product}/makes/utils/lint-python/settings-mypy.cfg"
       ];
       "python.linting.mypyEnabled" = true;
-      "python.linting.mypyPath" = "${packages.nixpkgs.mypy}/bin/mypy";
+      "python.linting.mypyPath" = "${packages.pythonOnNix.projects.mypy.latest.pythonLatest.bin}/bin/mypy";
       "python.linting.prospectorArgs" = [
         "--profile"
         "${sources.product}/makes/utils/lint-python/settings-prospector.yaml"
       ];
       "python.defaultInterpreterPath" = "/run/current-system/sw/bin/python3.8";
       "python.linting.prospectorEnabled" = true;
-      "python.linting.prospectorPath" = "prospector";
+      "python.linting.prospectorPath" = "${packages.pythonOnNix.projects.prospector.latest.pythonLatest.bin}/bin/prospector";
       "python.linting.pylintEnabled" = false;
       "python.pythonPath" = "${packages.nixpkgs.python38}/bin/python";
       "security.workspace.trust.enabled" = false;
